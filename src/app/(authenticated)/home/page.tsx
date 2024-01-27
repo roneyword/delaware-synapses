f9407b7436a4732d34b83c854ae9b79cf421e281
roney <roneyword@gmail.com>
2024-01-27 20:14:45 -0300

    create array projects

--- src/app/(authenticated)/home/page.tsx
+++ src/app/(authenticated)/home/page.tsx
@@ -3,12 +3,36 @@ import Card from "@/components/Card";
 import Header from "@/components/Header";
 import Wrapper from "@/components/Wrapper";
 
+const itens = [
+  {
+    isActive: true,
+    name: "Pré-projeto Browfield",
+    projectId: 1,
+    projectUuid: "95c97e4c-a32e-4917-b2c1-b828b35e9821",
+  },
+  {
+    isActive: true,
+    name: "Projeto S/4MOVE",
+    projectId: 1,
+    projectUuid: "95c97e4c-a32e-4917-b2c1-b828b35e9821",
+  },
+  {
+    isActive: true,
+    name: "Projeto Calix",
+    projectId: 1,
+    projectUuid: "95c97e4c-a32e-4917-b2c1-b828b35e9821",
+  },
+
+]
+
 export default function Home() {
   return (
     <BackgroundImg>
       <Header />
       <Wrapper>
-        <Card link="asdsad" title="Pré-projeto Browfield" />
+        {itens.map(project => (
+          <Card link={`control-center/${project.projectUuid}`} title={project.name} />
+        ))}
       </Wrapper>
     </BackgroundImg>
   );
